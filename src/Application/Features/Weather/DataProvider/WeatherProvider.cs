bb29db4a2606f681f9d60aeaf851c4a634459b32
Ermilov Aleksei <alex.ozr@gmail.com>
2024-10-15 16:37:35 +0300

    dotnet format

--- src/Application/Features/Weather/DataProvider/WeatherProvider.cs
+++ src/Application/Features/Weather/DataProvider/WeatherProvider.cs
@@ -1,6 +1,6 @@
 namespace TelegramBot.Application.Features.Weather.DataProvider;
 
-internal sealed class WeatherProvider: IWeatherProvider
+internal sealed class WeatherProvider : IWeatherProvider
 {
     public int GetTemperature(string city)
         => Random.Shared.Next(-10, 40);
